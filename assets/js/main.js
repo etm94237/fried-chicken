62cfa3599066b9162b1b85571ead60e54154b07c
etm94237@gmail.com <etm94237@gmail.com>
2025-07-29 20:31:26 -0400

    poulet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,33 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== ADD SHADOW HEADER ===============*/
-
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll',shadowHeader)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
